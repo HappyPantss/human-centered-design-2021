50c365b21763ca1cec2a78e74a230f82b9e4a62f
HappyPantss <sergioeijben@hotmail.com>
2020-11-17 17:35:47 +0100

    Vue.js Course

---
+++ frontend-applications/src/assets/HelloWorld.vue
@@ -0,0 +1,34 @@
+<!-- view -->
+<template>
+    <div>
+        <h1>My Fave Languages</h1>
+        <p>{{ keywords.join(' | ')}}</p>
+        <input :value="inputValue" @keyup.alt.enter="processInput($event)" />
+    </div>
+</template>
+
+<!-- logic -->
+<script>
+export default {
+    data() {
+        return {   
+            inputValue: "",
+            index: 0,
+            keywords: []
+        };
+    },
+    methods: {
+        processInput(event){
+            const value = event.target.value
+            this.keywords[this.index] = value;
+            this.inputValue = ''
+            this.index++
+        }
+    },
+}
+</script>
+
+<!-- style -->
+<style scoped>
+
+</style>
---
+++ frontend-applications/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './assets/HelloWorld.vue'
+
+const app = createApp(App)
+
+app.mount('#app')
