87f0d3fbad10900d7b28a71efcceb3959af4b746
HappyPantss <sergioeijben@hotmail.com>
2021-04-21 15:05:32 +0200

    :construction: WIP

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
-var colors = ['blue', 'brown', 'green', 'orange', 'purple'];
+var colors = ['blue', 'brown', 'red', 'purple'];
+var colorsNL = ['blauw', 'bruin', 'oranje', 'paars'];
 var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
 
 var recognition = new SpeechRecognition();
@@ -10,39 +11,40 @@ var speechRecognitionList = new SpeechGrammarList();
 speechRecognitionList.addFromString(grammar, 1);
 recognition.grammars = speechRecognitionList;
 recognition.continuous = false;
-recognition.lang = 'en-US';
+recognition.lang = 'nl-NL';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
 var diagnostic = document.querySelector('.output');
-var bg = document.querySelector('html');
+var bg = document.querySelector('body');
 var hints = document.querySelector('.hints');
 
 var colorHTML = '';
-colors.forEach(function(v, i, a) {
-    console.log(v, i);
-    colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
-});
-hints.innerHTML = 'Zeg een kleur in het Engels om de achtergrondkleur van de app te wijzigen. Probeer ' + colorHTML + '.';
+hints.innerHTML = 'Zeg een kleur om de achtergrondkleur van de app te wijzigen. Probeer ' + colorHTML + '.';
 
-document.body.onload = function() {
+function handleBtnClick() {
     recognition.start();
     console.log('Klaar om een kleuropdracht te ontvangen.');
 }
 
+// document.body.onload = function() {
+//     recognition.start();
+//     console.log('Klaar om een kleuropdracht te ontvangen.');
+// }
+
 recognition.onresult = function(event) {
-    // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-    // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-    // It has a getter so it can be accessed like an array
-    // The first [0] returns the SpeechRecognitionResult at the last position.
-    // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-    // These also have getters so they can be accessed like arrays.
-    // The second [0] returns the SpeechRecognitionAlternative at position 0.
-    // We then return the transcript property of the SpeechRecognitionAlternative object
     var color = event.results[0][0].transcript;
     diagnostic.textContent = 'Resultaat ontvangen: ' + color + '.';
+    if (color == 'blauw' || color == 'Blauw') {
+        bg.style.backgroundColor = 'blue';
+    } else if (color == 'bruin' || color == 'Bruin') {
+        bg.style.backgroundColor = '#482f2b';
+    } else if (color == 'rood' || color == 'Rood') {
+        bg.style.backgroundColor = '#c0392b';
+    } else if (color == 'paars' || color == 'Paars') {
+        bg.style.backgroundColor = '#8e44ad';
+    }
     bg.style.backgroundColor = color;
-    document.getElementsByTagName("body")[0].style.backgroundColor = color;
     console.log('Vertrouwen: ' + event.results[0][0].confidence);
 }
 
@@ -50,7 +52,7 @@ recognition.onspeechend = function() {
     recognition.stop();
 }
 
-recognition.onnomatch = function(event) {
+recognition.onnomatch = function() {
     diagnostic.textContent = "Ik herkende die kleur niet.";
 }
 
