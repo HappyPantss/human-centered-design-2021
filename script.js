ac4ca123ffdf951216d28c656518b348a15773e3
HappyPantss <sergioeijben@hotmail.com>
2021-04-29 01:06:15 +0200

    :construction: WIP Adding live data (date, time and temperature

--- script.js
+++ script.js
@@ -25,7 +25,12 @@ var hints = document.querySelector('.hints')
 
 function handleBtnClick() {
     recognition.start()
-    document.getElementById('my_audio3').play()
+
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = "Spreek je plaats in.";
+    msg.lang = 'nl';
+    speechSynthesis.speak(msg);
+
     voiceWave.style.display = 'block'
     voiceBtn.style.display = 'none'
     console.log('Klaar om een kleuropdracht te ontvangen.');
@@ -45,29 +50,104 @@ recognition.onresult = function(event) {
     var color = event.results[0][0].transcript;
     diagnostic.textContent = 'Resultaat ontvangen: ' + color + '.'
     console.log('Vertrouwen: ' + event.results[0][0].confidence)
+
+    const API_KEY = '6360ba36cb9146e6cdcac987c838d764'
+        // const locatie = 'Sal'
+        // const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${CITY}&units=metric&appid=${API_KEY}`
+        // http://api.openweathermap.org/data/2.5/weather?q=Amsterdam&units=metric&appid=6360ba36cb9146e6cdcac987c838d764
+
+    function getData() {
+        return new Promise((resolve, reject) => {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=` + color + `&units=metric&appid=` + API_KEY)
+                .then(response => {
+                    if (!response.ok) {
+                        throw Error("ERROR")
+                    }
+                    return response.json()
+                })
+                .then(data => {
+                    let html = data.main.temp
+                    console.log(html)
+                    resolve(html)
+
+                    var temperatuur = Math.floor(html);
+
+                    let typeWeer
+                    if (temperatuur < 10 || temperatuur > 25) {
+                        typeWeer == 'slecht'
+                    } else if (temperatuur >= 10 && temperatuur <= 15) {
+                        typeWeer == 'perfect'
+                    } else if (temperatuur >= 15 && temperatuur <= 20) {
+                        typeWeer == 'prima'
+                    } else if (temperatuur >= 20 && temperatuur <= 25) {
+                        typeWeer == 'perfect'
+                    }
+
+                    var msg = new SpeechSynthesisUtterance();
+                    msg.text = welkom + "het is " + datumweergave + " en " + tijdweergave + " uur. De temperatuur in " + color + " is " + temperatuur + " graden. Het is " + typeWeer + " weer om buiten te gaan hardlopen.";
+                    msg.lang = 'nl';
+                    speechSynthesis.speak(msg);
+
+                    hints.innerHTML = welkom + "het is " + datumweergave + " en " + tijdweergave + " uur. De temperatuur in " + color + " is " + temperatuur + " graden. Het is " + typeWeer + " weer om buiten te gaan hardlopen.";
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        })
+    }
+
+    getData()
 }
+
+// Bereken de dag en het uur.
+function initArray() {
+    this.length = initArray.arguments.length;
+    for (var i = 0; i < this.length; i++)
+        this[i + 1] = initArray.arguments[i];
+}
+
+var dagArray = new initArray("zondag", "maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag");
+
+var maandArray = new initArray("januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december");
+
+var nu = new Date();
+var dagtekst = dagArray[(nu.getDay() + 1)];
+var dag = nu.getDate();
+var maandtekst = maandArray[(nu.getMonth() + 1)];
+var jaar = nu.getYear();
+var jaar4 = ((jaar < 1900) ? (jaar + 1900) : (jaar));
+
+var datumweergave = dagtekst + " " + dag + " " + maandtekst + " " + jaar4;
+
+var uren = nu.getHours();
+var uren2 = ((uren < 10) ? "0" : "") + uren;
+var minuten = nu.getMinutes();
+var minuten2 = ((minuten < 10) ? ":0" : ":") + minuten;
+
+var tijdweergave = uren2 + minuten2
+
+if (nu.getHours() >= 18)
+    var welkom = ("Goedenavond, ");
+else if (nu.getHours() >= 12)
+    var welkom = ("Goedemiddag, ");
+else if (nu.getHours() >= 6)
+    var welkom = ("Goedemorgen, ");
+else
+    var welkom = ("Goedenacht, ");
+
+// var bericht = welkom + "het is " + datumweergave + " en " + tijdweergave + " uur.";
 
 recognition.onspeechend = function() {
     recognition.stop();
     voiceWave.style.display = 'none'
     voiceBtn.style.display = 'block'
 
-    document.getElementById('my_audio4').play()
-    setTimeout(function() {
-        document.getElementById('my_audio5').play()
-    }, 2500)
-    setTimeout(function() {
-        document.getElementById('my_audio6').play()
-    }, 5500)
-
-    hints.innerHTML = "Het is 6 over 4 's middags. De temperatuur in Amsterdam is 15 graden. Het is prima weer om buiten te gaan hardlopen"
-
     voiceBtn.innerHTML = 'Spreek een andere plaats in'
     question.style.display = 'none'
     title.style.display = 'none'
     setTimeout(function() {
         repeatDiv.style.display = 'block'
-    }, 7500)
+    }, 12000)
 }
 
 recognition.onnomatch = function() {
